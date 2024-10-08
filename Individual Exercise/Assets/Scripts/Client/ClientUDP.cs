d15830b1cf954f64e483b006a616ae2ea28b375c
Marta <73582921+Marta-24@users.noreply.github.com>
2024-10-08 21:29:42 +0200

    Minimum Required Done :D

---
+++ Individual Exercise/Assets/Scripts/Client/ClientUDP.cs
@@ -0,0 +1,108 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using System.Threading;
+using TMPro;
+
+public class ClientUDP : MonoBehaviour
+{
+    Socket socket;
+    public GameObject UItextObj;
+    public GameObject ipInputField;
+    TextMeshProUGUI UItext;
+    string clientText;
+
+    void Start()
+    {
+        if (UItextObj != null)
+        {
+            UItext = UItextObj.GetComponent<TextMeshProUGUI>();
+        }
+        else
+        {
+            Debug.LogError("UItextObj is not assigned in the Inspector!");
+        }
+    }
+
+    public void StartClient()
+    {
+        if (ipInputField == null)
+        {
+            Debug.LogError("ipInputField is not assigned in the Inspector!");
+            return;
+        }
+
+        // Get the TMP_InputField component from the ipInputField GameObject
+        TMP_InputField inputField = ipInputField.GetComponent<TMP_InputField>();
+
+        if (inputField == null)
+        {
+            Debug.LogError("ipInputField does not contain a TMP_InputField component!");
+            return;
+        }
+
+        // Use the text from the input field
+        string serverIP = inputField.text;
+
+        // Start a thread to send a message to the server
+        Thread mainThread = new Thread(Send);
+        mainThread.Start();
+    }
+
+
+
+    void Update()
+    {
+        UItext.text = clientText;
+    }
+
+    void Send()
+    {
+        // TO DO 2
+        // Create the server's endpoint (IP and port) and initialize the socket
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050); // Replace with your server's IP and port
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // UDP socket
+
+        // TO DO 2.1
+        // Send the Handshake to the server's endpoint
+        string handshake = "Hello World";
+        byte[] data = Encoding.ASCII.GetBytes(handshake);
+        socket.SendTo(data, data.Length, SocketFlags.None, ipep); // Send handshake to server
+
+        clientText = "Sent handshake to server: " + handshake;
+
+        // TO DO 5
+        // Start a thread to receive the response from the server
+        Thread receive = new Thread(Receive);
+        receive.Start();
+    }
+
+    void Receive()
+    {
+        // TO DO 5
+        // Prepare to receive data from the server
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0); // Sender (server) endpoint
+        EndPoint Remote = (EndPoint)sender;
+
+        byte[] data = new byte[1024]; // Buffer to store received data
+        int recv;
+
+        // Receive data from the server
+        try
+        {
+            recv = socket.ReceiveFrom(data, ref Remote);
+            string receivedMessage = Encoding.ASCII.GetString(data, 0, recv); // Convert received data to string
+
+            // Display the received message
+            clientText = $"Message received from {Remote.ToString()}: {receivedMessage}";
+        }
+        catch (SocketException e)
+        {
+            clientText = "Error receiving message: " + e.Message;
+        }
+
+        // Optionally, you can close the socket if done
+        socket.Close();
+    }
+}
---
+++ Individual Exercise/Assets/Scripts/Server/ServerTCP.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using TMPro;
+using System.Text;
+
+public class ServerTCP : MonoBehaviour
+{
+    Socket socket;
+    Thread mainThread = null;
+
+    public GameObject UItextObj;
+    TextMeshProUGUI UItext;
+    string serverText;
+
+    public struct User
+    {
+        public string name;
+        public Socket socket;
+    }
+
+    void Start()
+    {
+        UItext = UItextObj.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        UItext.text = serverText;
+    }
+
+    public void startServer()
+    {
+        serverText = "Starting TCP Server...";
+
+        // Create and bind the socket
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to any IP address on port 9050
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 9050);
+        socket.Bind(localEndPoint);
+
+        // Put the socket in listening mode
+        socket.Listen(10);
+        serverText += "\nServer listening on port 9050...";
+
+        // Start a thread to check for new connections
+        mainThread = new Thread(CheckNewConnections);
+        mainThread.Start();
+    }
+
+    void CheckNewConnections()
+    {
+        while (true)
+        {
+            User newUser = new User();
+            newUser.name = "";
+
+            // Accept any incoming clients
+            newUser.socket = socket.Accept();
+
+            // Get the remote endpoint (client info)
+            IPEndPoint clientEP = (IPEndPoint)newUser.socket.RemoteEndPoint;
+            serverText += "\nConnected with " + clientEP.Address.ToString() + " at port " + clientEP.Port.ToString();
+
+            // Start a new thread to handle messages from this client
+            Thread newConnection = new Thread(() => Receive(newUser));
+            newConnection.Start();
+        }
+    }
+
+    void Receive(User user)
+    {
+        byte[] data = new byte[1024];
+        int recv;
+
+        while (true)
+        {
+            try
+            {
+                recv = user.socket.Receive(data);
+                if (recv == 0)
+                    break;
+
+                // Convert received data to string
+                string receivedMessage = Encoding.ASCII.GetString(data, 0, recv);
+                serverText += "\nReceived: " + receivedMessage;
+
+                // Send a ping back to the client
+                Send(user);
+            }
+            catch (SocketException)
+            {
+                // Handle socket exceptions
+                break;
+            }
+        }
+
+        // Close the socket when done
+        user.socket.Close();
+    }
+
+    void Send(User user)
+    {
+        string pingMessage = "Ping";
+        byte[] data = Encoding.ASCII.GetBytes(pingMessage);
+        user.socket.Send(data); // Send to the client directly via their socket
+        serverText += "\nSent ping to client: " + pingMessage;
+    }
+}
